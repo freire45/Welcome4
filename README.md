7c3f93123c9a3306c9d8e221dc7a3c3dcc18d6d9
Erick Freire <erilefr@gmail.com>
2022-10-03 19:55:00 -0300

    a
    a

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Welcome4
  Welcome To Java Programming ! Version 4
+
+ ## Image
+![Currículo no Computador](welcome_4.png)
---
+++ Welcome4/src/br/com/erickfreire/welcome4/Welcome4.java
@@ -0,0 +1,15 @@
+package br.com.erickfreire.welcome4;
+
+/**
+ * Welcome to Java Programming! Version 4
+ * @author Erick Freire
+ * @version 1 - Created in 10-03-2022
+ */
+
+public class Welcome4 {
+
+	public static void main(String[] args) {
+		System.out.printf("%s%n%s%n", "Welcome to", "Java Programming!");
+	}
+
+}
